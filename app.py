157e67c5d70c7a148c2df67442c962a6631c8757
joe901007 <107670542+joe901007@users.noreply.github.com>
2022-06-17 16:14:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -145,8 +145,7 @@ def handle_message(event):
 
 
         message = TextSendMessage(text=string)
-    else:
-        message = TextSendMessage(text=event.message.text)
+        line_bot_api.reply_message(event.reply_token, message)
 
 #主程式
 import os
